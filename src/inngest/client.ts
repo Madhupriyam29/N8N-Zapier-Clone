5703553ccf549f03e311d9b8214bd5abe667e269
Madhupriyam29 <madhupriyam2000@gmail.com>
2025-10-23 13:15:16 +0530

    added inngest event key and inngest signing key in innest/client.ts

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -2,6 +2,6 @@ import { Inngest } from "inngest";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "nodebase" ,
-    // eventKey: process.env.INNGEST_EVENT_KEY,
+    eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY
 });
